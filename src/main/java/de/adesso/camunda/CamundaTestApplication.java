d35e9fb52046e58b551ad96a63194132fdc32a2d
Metten, Fabian <Fabian.Metten@adesso.de>
2021-01-25 09:05:03 +0100

    Initialer Commit

---
+++ src/main/java/de/adesso/camunda/CamundaTestApplication.java
@@ -0,0 +1,13 @@
+package de.adesso.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaTestApplication.class, args);
+	}
+
+}
